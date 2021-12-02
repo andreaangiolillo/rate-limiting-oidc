e0e93d8099feac7051d47c93645618fef5883b9f
andreaangiolillo <andrea.angiolillo@mongodb.com>
2021-12-02 11:38:23 +0000

    Refactoring

--- handler/handler.go
+++ handler/handler.go
@@ -1,58 +1,37 @@
+// Copyright 2021 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package handler
 
 import (
-	"bytes"
-	"encoding/base64"
-	"encoding/json"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"net/http"
+	"okta-hosted-login/m/store"
 	"os"
 
 	verifier "github.com/okta/okta-jwt-verifier-golang"
-	"okta-hosted-login/m/store"
 )
-
 
 var s = store.New()
 
-type Exchange struct {
-	Error            string `json:"error,omitempty"`
-	ErrorDescription string `json:"error_description,omitempty"`
-	AccessToken      string `json:"access_token,omitempty"`
-	TokenType        string `json:"token_type,omitempty"`
-	ExpiresIn        int    `json:"expires_in,omitempty"`
-	Scope            string `json:"scope,omitempty"`
-	IdToken          string `json:"id_token,omitempty"`
-}
-
-type Profile struct {
-	Sub               string `json:"sub,omitempty"`
-	Name              string `json:"name,omitempty"`
-	Locale            string `json:"locale,omitempty"`
-	Email             string `json:"email,omitempty"`
-	PreferredUsername string `json:"preferred_username,omitempty"`
-	GivenName         string `json:"given_name,omitempty"`
-	FamilyName        string `json:"family_name,omitempty"`
-	ZoneInfo          string `json:"zoneinfo,omitempty"`
-	UpdatedAt         int    `json:"updated_at,omitempty"`
-	EmailVerified     bool   `json:"email_verified,omitempty"`
-	LastName          string `json:"lastName,omitempty"`
-	FirstName         string `json:"firstName,omitempty"`
-	Test              string `json:"test,omitempty"`
-	Login             string `json:"login,omitempty"`
-	PrimaryEmail      string `json:"primaryEmail,omitempty"`
-}
-
 type customData struct {
-	Profile         *Profile
+	Profile         *store.Profile
 	IsAuthenticated bool
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-
-	profile, err := profileData(r)
+	profile, err := s.ProfileDataRequest(r)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
@@ -71,25 +50,11 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Cache-Control", "no-cache") // See https://github.com/okta/samples-golang/issues/20
-
-	var redirectPath string
-
-	q := r.URL.Query()
-	q.Add("client_id", os.Getenv("CLIENT_ID"))
-	q.Add("response_type", "code")
-	q.Add("response_mode", "query")
-	q.Add("scope", "openid profile email")
-	q.Add("redirect_uri", "http://localhost:8080/authorization-code/callback")
-	q.Add("state", s.State)
-	q.Add("nonce", s.Nonce)
-
-	redirectPath = os.Getenv("ISSUER") + "/v1/authorize?" + q.Encode()
-
-	http.Redirect(w, r, redirectPath, http.StatusFound)
+	http.Redirect(w, r, s.AuthorizationCodeRequest(r), http.StatusFound)
 }
 
 func AuthCodeCallbackHandler(w http.ResponseWriter, r *http.Request) {
-	// Check the state that was returned in the query string is the same as the above state
+	// Check the state that was returned to the query string is the same as the above state
 	if r.URL.Query().Get("state") != s.State {
 		fmt.Println(w, "The state was not as expected")
 		http.Error(w, "The state was not as expected", http.StatusInternalServerError)
@@ -100,7 +65,7 @@ func AuthCodeCallbackHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "The code was not returned or is not accessible", http.StatusInternalServerError)
 	}
 
-	exchange, err := exchangeCode(r.URL.Query().Get("code"), r)
+	exchange, err := s.ExchangeCodeRequest(r.URL.Query().Get("code"), r)
 	if exchange.Error != "" || err != nil {
 		fmt.Println(exchange.Error)
 		fmt.Println(exchange.ErrorDescription)
@@ -132,17 +97,19 @@ func AuthCodeCallbackHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func ProfileHandler(w http.ResponseWriter, r *http.Request) {
-	profile, err := profileData(r)
+	profile, err := s.ProfileDataRequest(r)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+
 	data := customData{
 		Profile:         profile,
 		IsAuthenticated: isAuthenticated(r),
 	}
+
 	err = s.TPL.ExecuteTemplate(w, "profile.gohtml", data)
 	if err != nil {
-		return
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
@@ -163,86 +130,12 @@ func LogoutHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/", http.StatusFound)
 }
 
-func exchangeCode(code string, r *http.Request) (*Exchange, error) {
-	authHeader := base64.StdEncoding.EncodeToString(
-		[]byte(os.Getenv("CLIENT_ID") + ":" + os.Getenv("CLIENT_SECRET")))
-
-	q := r.URL.Query()
-	q.Add("grant_type", "authorization_code")
-	q.Set("code", code)
-	q.Add("redirect_uri", "http://localhost:8080/authorization-code/callback")
-
-	url := os.Getenv("ISSUER") + "/v1/token?" + q.Encode()
-
-	req, _ := http.NewRequest("POST", url, bytes.NewReader([]byte("")))
-	h := req.Header
-	h.Add("Authorization", "Basic "+authHeader)
-	h.Add("Accept", "application/json")
-	h.Add("Content-Type", "application/x-www-form-urlencoded")
-	h.Add("Connection", "close")
-	h.Add("Content-Length", "0")
-
-	client := &http.Client{}
-	resp, _ := client.Do(req)
-	body, _ := ioutil.ReadAll(resp.Body)
-
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-
-		}
-	}(resp.Body)
-
-	var exchange Exchange
-	err := json.Unmarshal(body, &exchange)
-	if err != nil {
-		return nil, err
-	}
-
-	return &exchange, nil
-}
-
 func isAuthenticated(r *http.Request) bool {
 	session, err := s.Session.Session(r)
-
 	if err != nil || session.Values["id_token"] == nil || session.Values["id_token"] == "" {
 		return false
 	}
-
 	return true
-}
-
-func profileData(r *http.Request) (*Profile, error) {
-	var m *Profile
-	session, err := s.Session.Session(r)
-
-	if err != nil || session.Values["access_token"] == nil || session.Values["access_token"] == "" {
-		return m, nil
-	}
-
-	reqUrl := os.Getenv("ISSUER") + "/v1/userinfo"
-
-	req, _ := http.NewRequest("GET", reqUrl, bytes.NewReader([]byte("")))
-	h := req.Header
-	h.Add("Authorization", "Bearer "+session.Values["access_token"].(string))
-	h.Add("Accept", "application/json")
-
-	client := &http.Client{}
-	resp, _ := client.Do(req)
-	body, _ := ioutil.ReadAll(resp.Body)
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-
-		}
-	}(resp.Body)
-
-	err = json.Unmarshal(body, &m)
-	if err != nil {
-		return nil, err
-	}
-
-	return m, nil
 }
 
 func verifyToken(t string) (*verifier.Jwt, error) {
--- store/store.go
+++ store/store.go
@@ -1,4 +1,4 @@
-// Copyright 2020 MongoDB Inc
+// Copyright 2021 MongoDB Inc
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,8 +15,15 @@
 package store
 
 import (
+	"bytes"
+	"encoding/base64"
 	"encoding/hex"
+	"encoding/json"
 	"html/template"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
 
 type Store struct {
@@ -24,6 +31,34 @@ type Store struct {
 	TPL     *template.Template
 	Nonce   string
 	State   string
+}
+
+type Exchange struct {
+	Error            string `json:"error,omitempty"`
+	ErrorDescription string `json:"error_description,omitempty"`
+	AccessToken      string `json:"access_token,omitempty"`
+	TokenType        string `json:"token_type,omitempty"`
+	ExpiresIn        int    `json:"expires_in,omitempty"`
+	Scope            string `json:"scope,omitempty"`
+	IdToken          string `json:"id_token,omitempty"`
+}
+
+type Profile struct {
+	Sub               string `json:"sub,omitempty"`
+	Name              string `json:"name,omitempty"`
+	Locale            string `json:"locale,omitempty"`
+	Email             string `json:"email,omitempty"`
+	PreferredUsername string `json:"preferred_username,omitempty"`
+	GivenName         string `json:"given_name,omitempty"`
+	FamilyName        string `json:"family_name,omitempty"`
+	ZoneInfo          string `json:"zoneinfo,omitempty"`
+	UpdatedAt         int    `json:"updated_at,omitempty"`
+	EmailVerified     bool   `json:"email_verified,omitempty"`
+	LastName          string `json:"lastName,omitempty"`
+	FirstName         string `json:"firstName,omitempty"`
+	Test              string `json:"test,omitempty"`
+	Login             string `json:"login,omitempty"`
+	PrimaryEmail      string `json:"primaryEmail,omitempty"`
 }
 
 func New() *Store {
@@ -39,4 +74,90 @@ func generateState() string {
 	// Generate a random byte array for state parameter
 	b := make([]byte, 16)
 	return hex.EncodeToString(b)
+}
+
+func (s *Store) AuthorizationCodeRequest(r *http.Request) string {
+	// Endpoint: https://developer.okta.com/docs/reference/api/oidc/#authorize
+	q := r.URL.Query()
+	q.Add("client_id", os.Getenv("CLIENT_ID"))
+	q.Add("response_type", "code")
+	q.Add("response_mode", "query")
+	q.Add("scope", "openid profile email")
+	q.Add("redirect_uri", "http://localhost:8080/authorization-code/callback")
+	q.Add("state", s.State)
+	q.Add("nonce", s.Nonce)
+	return os.Getenv("ISSUER") + "/v1/authorize?" + q.Encode()
+}
+
+func (s *Store) ExchangeCodeRequest(code string, r *http.Request) (*Exchange, error) {
+	// Endpoint https://developer.okta.com/docs/reference/api/oidc/#token
+	authHeader := base64.StdEncoding.EncodeToString(
+		[]byte(os.Getenv("CLIENT_ID") + ":" + os.Getenv("CLIENT_SECRET")))
+
+	q := r.URL.Query()
+	q.Add("grant_type", "authorization_code")
+	q.Set("code", code)
+	q.Add("redirect_uri", "http://localhost:8080/authorization-code/callback")
+
+	url := os.Getenv("ISSUER") + "/v1/token?" + q.Encode()
+
+	req, _ := http.NewRequest("POST", url, bytes.NewReader([]byte("")))
+	h := req.Header
+	h.Add("Authorization", "Basic "+authHeader)
+	h.Add("Accept", "application/json")
+	h.Add("Content-Type", "application/x-www-form-urlencoded")
+	h.Add("Connection", "close")
+	h.Add("Content-Length", "0")
+
+	client := &http.Client{}
+	resp, _ := client.Do(req)
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+
+		}
+	}(resp.Body)
+
+	var exchange Exchange
+	err := json.Unmarshal(body, &exchange)
+	if err != nil {
+		return nil, err
+	}
+
+	return &exchange, nil
+}
+
+func (s *Store) ProfileDataRequest(r *http.Request) (*Profile, error) {
+	// Endpoint: https://developer.okta.com/docs/reference/api/oidc/#userinfo
+	var profile *Profile
+	session, err := s.Session.Session(r)
+
+	if err != nil || session.Values["access_token"] == nil || session.Values["access_token"] == "" {
+		return profile, nil
+	}
+
+	reqUrl := os.Getenv("ISSUER") + "/v1/userinfo"
+	req, _ := http.NewRequest("GET", reqUrl, bytes.NewReader([]byte("")))
+	h := req.Header
+	h.Add("Authorization", "Bearer "+session.Values["access_token"].(string))
+	h.Add("Accept", "application/json")
+
+	client := &http.Client{}
+	resp, _ := client.Do(req)
+	body, _ := ioutil.ReadAll(resp.Body)
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+
+		}
+	}(resp.Body)
+
+	err = json.Unmarshal(body, &profile)
+	if err != nil {
+		return nil, err
+	}
+
+	return profile, nil
 }
